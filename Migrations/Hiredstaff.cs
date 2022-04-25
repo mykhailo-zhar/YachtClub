d39c54f1ff82df01d8b9040b1b865350cee180a4
MrZahn <mykhailo.zhar@stud.onu.edu.ua>
2022-04-25 14:37:06 +0300

    Before another scaffold

---
+++ Migrations/Hiredstaff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Migrations
+{
+    [Table("hiredstaff")]
+    public partial class Hiredstaff
+    {
+        [Key]
+        [Column("yacht_crewid")]
+        public int YachtCrewid { get; set; }
+        [Key]
+        [Column("clientid")]
+        public int Clientid { get; set; }
+
+        [ForeignKey(nameof(Clientid))]
+        [InverseProperty(nameof(Person.Hiredstaff))]
+        public virtual Person Client { get; set; }
+        [ForeignKey(nameof(YachtCrewid))]
+        [InverseProperty("Hiredstaff")]
+        public virtual YachtCrew YachtCrew { get; set; }
+    }
+}
---
+++ Migrations/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Migrations
+{
+    [Table("person")]
+    public partial class Person
+    {
+        public Person()
+        {
+            Contract = new HashSet<Contract>();
+            Hiredstaff = new HashSet<Hiredstaff>();
+            Review = new HashSet<Review>();
+            StaffPosition = new HashSet<StaffPosition>();
+            Yacht = new HashSet<Yacht>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required]
+        [Column("name", TypeName = "character varying")]
+        public string Name { get; set; }
+        [Required]
+        [Column("surname", TypeName = "character varying")]
+        public string Surname { get; set; }
+        [Column("birthdate")]
+        public DateTime Birthdate { get; set; }
+        [Required]
+        [Column("sex", TypeName = "character varying")]
+        public string Sex { get; set; }
+        [Required]
+        [Column("email", TypeName = "character varying")]
+        public string Email { get; set; }
+        [Required]
+        [Column("phone", TypeName = "character varying")]
+        public string Phone { get; set; }
+        [Column("registrydate")]
+        public DateTime Registrydate { get; set; }
+
+        [InverseProperty("Client")]
+        public virtual ICollection<Contract> Contract { get; set; }
+        [InverseProperty("Client")]
+        public virtual ICollection<Hiredstaff> Hiredstaff { get; set; }
+        [InverseProperty("Client")]
+        public virtual ICollection<Review> Review { get; set; }
+        [InverseProperty("Staff")]
+        public virtual ICollection<StaffPosition> StaffPosition { get; set; }
+        [InverseProperty("Yachtowner")]
+        public virtual ICollection<Yacht> Yacht { get; set; }
+    }
+}
